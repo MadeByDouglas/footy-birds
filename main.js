0650231f57defea6f9b670481e6e0ef8b4af579d
Douglas Hewitt <doughewitt4@gmail.com>
2021-04-21 03:28:48 -0700

    parallax FTW
    to the moon!

--- main.js
+++ main.js
@@ -25,3 +25,23 @@ const app = Vue.createApp({
         }
     }
 })
+
+
+// parallax
+const parallaxEls = document.querySelectorAll("[data-speed]");
+ 
+window.addEventListener("scroll", scrollHandler);
+ 
+function scrollHandler() {
+  for (const parallaxEl of parallaxEls) {
+    const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
+    const transformY = this.pageYOffset * parallaxEl.dataset.speed;
+    if (parallaxEl.classList.contains("banner-title")) {
+      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-6deg)`;
+    } else if (parallaxEl.classList.contains("banner-subtitle")) {
+      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-3deg)`;
+    } else {
+      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
+    }
+  }
+}
