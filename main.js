e2a34e6f9bb7aa94b4a096cfd5608130851b366f
Douglas Hewitt <doughewitt4@gmail.com>
2021-04-21 04:36:51 -0700

    make the cart actually kinda work
    at least with the one product we plug into the system
    
    also fix banner image styling on about page

--- main.js
+++ main.js
@@ -18,30 +18,61 @@ function displayNewPage(newPage) {
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Tree Striker',
-            tagline: 'Control and Style',
-            price: '$129',
-            image: './images/merc-1.jpg'
+            product: '',
+            color: '',
+            tagline: '',
+            price: '',
+            image: '',
+
         }
     }
-})
+});
+
+function setCart(newProduct) {
+    mountedApp.product = newProduct[0];
+    mountedApp.color = newProduct[1];
+    mountedApp.tagline = newProduct[2];
+    mountedApp.price = newProduct[3];
+    mountedApp.image = newProduct[4];
+
+    displayNewPage('cart');
+}
+
+function checkout() {
+    if (mountedApp.product != "") {
+        displayNewPage('checkout');
+    } else {
+        window.alert("Please add a product to your cart");
+    }
+}
+
+function purchase() {
+    // clear the cart
+    mountedApp.product = '';
+    mountedApp.color = '';
+    mountedApp.tagline = '';
+    mountedApp.price = '';
+    mountedApp.image = '';
+    
+    displayNewPage('thankyou');
+}
 
 
 // parallax
 const parallaxEls = document.querySelectorAll("[data-speed]");
- 
+
 window.addEventListener("scroll", scrollHandler);
- 
+
 function scrollHandler() {
-  for (const parallaxEl of parallaxEls) {
-    const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
-    const transformY = this.pageYOffset * parallaxEl.dataset.speed;
-    if (parallaxEl.classList.contains("banner-title")) {
-      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-6deg)`;
-    } else if (parallaxEl.classList.contains("banner-subtitle")) {
-      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-3deg)`;
-    } else {
-      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
+        for (const parallaxEl of parallaxEls) {
+        const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
+        const transformY = this.pageYOffset * parallaxEl.dataset.speed;
+        if (parallaxEl.classList.contains("banner-title")) {
+            parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-6deg)`;
+        } else if (parallaxEl.classList.contains("banner-subtitle")) {
+            parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-3deg)`;
+        } else {
+            parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
+        }
     }
-  }
 }
